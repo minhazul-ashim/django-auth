95e7345de170d8fff92df309c31cedb675f23a81
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-03-25 17:24:23 +0600

    templates added

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views;
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('login/', views.login),
+    path('register/', views.register),
+    path('profile/', views.profile)
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request) :
+    return render(request, 'register.html');
+
+def login(request) :
+    return render(request, 'login.html');
+
+def profile(request) :
+    return render(request, 'profile.html');
